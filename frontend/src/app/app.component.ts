1fc8e67118d89630493d402316b054236370afc8
kr1pt0n05 <sandrolipinski@outlook.com>
2025-05-04 18:49:54 +0200

    Adjusted Navbar. Needs styling & logo
    - Hardcoded navbar routes
    - Added NotFound route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from './components/navbar/navbar.component';
+import {Component, signal} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    NavbarComponent,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  currentPageTitle = signal<string>("Homepage");
+
+  updateCurrentPageTitle(currentPageTitle: string) {
+    this.currentPageTitle.set(currentPageTitle);
+  }
 
 }
