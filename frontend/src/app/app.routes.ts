05c9132c5e11fb2faf8e48d48d090a444a177894
kr1pt0n05 <sandrolipinski@outlook.com>
2025-05-04 14:01:38 +0200

    Created navbar component and added logic
    Dynamically renders navbar elements, depending on routes defined in src/app/app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {InventoryComponent} from './pages/inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inventory',
+    component: InventoryComponent,
+  }
+];
