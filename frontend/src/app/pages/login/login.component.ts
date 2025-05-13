7c510abba510f664a8d53548fbb774d45d622094
kr1pt0n05 <sandrolipinski@outlook.com>
2025-05-13 20:01:12 +0200

    Created Dummy Login Page

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {CardComponent} from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
