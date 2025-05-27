f3cfad2b290e884f0d4494ff6fdaf0f1b5eb6111
Throin <xthroin@gmail.com>
2025-05-28 01:47:53 +0200

    Fix: Add HTTP client providers to service tests for proper dependency injection

--- frontend/src/app/services/inventories.service.spec.ts
+++ frontend/src/app/services/inventories.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { InventoriesService } from './inventories.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('InventoriesService', () => {
   let service: InventoriesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(InventoriesService);
   });
 
--- frontend/src/app/services/server-table-data-source.service.spec.ts
+++ frontend/src/app/services/server-table-data-source.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ServerTableDataSourceService } from './server-table-data-source.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ServerTableDataSourceService', () => {
-  let service: ServerTableDataSourceService;
+  let service: ServerTableDataSourceService<any>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ServerTableDataSourceService);
   });
 
