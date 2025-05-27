d0c6fada7b8faf7f1574e3f40c67835deedc8096
Dominik Ehmann <dome.ehmann@gmail.com>
2025-05-27 16:12:07 +0200

    Add files via upload

--- backend/src/main/java/com/hs_esslingen/insy/dto/ExtensionsResponseDTO.java
+++ backend/src/main/java/com/hs_esslingen/insy/dto/ExtensionsResponseDTO.java
@@ -1,22 +1,24 @@
 package com.hs_esslingen.insy.dto;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class Comment {
+public class ExtensionsResponseDTO {
     private Integer id;
+
     private String description;
+
+    private String company;
+
+    private BigDecimal price;
+
+    private String serial_number;
+
     @JsonFormat(pattern = "dd.MM.yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
-    private LocalDateTime createdAt;
-    private String author;
+    private LocalDateTime created_at;
 }
---
+++ backend/src/main/java/com/hs_esslingen/insy/dto/InventoriesCreateRequestDTO.java
@@ -0,0 +1,41 @@
+package com.hs_esslingen.insy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+public class InventoriesCreateRequestDTO {
+
+    @JsonProperty("cost_center")
+    public String costCenter;
+
+    @JsonProperty("inventories_id")
+    public Integer inventoriesId;
+
+    @JsonProperty("inventories_description")
+    public String description;
+
+    @JsonProperty("company")
+    public String company;
+
+    @JsonProperty("inventories_price")
+    public BigDecimal price;
+
+    @JsonProperty("inventories_serial_number")
+    public String serialNumber;
+
+    @JsonProperty("inventories_location")
+    public String location;
+
+    @JsonProperty("orderer")
+    public Object orderer; // weil "orderer" int **oder** String sein kann
+
+    @JsonProperty("tags")
+    public List<Integer> tags;
+}
