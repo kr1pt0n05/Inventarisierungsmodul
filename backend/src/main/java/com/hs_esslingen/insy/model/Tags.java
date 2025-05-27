4bb58f7acdcaae4e97926303a7adb91ca9da0901
akkait00 <akimk00@gmail.com>
2025-05-27 18:10:50 +0200

    feat: added for Tags

--- backend/src/main/java/com/hs_esslingen/insy/model/Tags.java
+++ backend/src/main/java/com/hs_esslingen/insy/model/Tags.java
@@ -3,48 +3,66 @@ package com.hs_esslingen.insy.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.*;
 
-
-@AllArgsConstructor
-@Data
 @Entity
-@Builder
-@Getter
-@Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "tags")
 public class Tags {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)    
     @Column(nullable = false)
     private Integer id;
 
     @Column(nullable = false)
     private String name;
 
-    @ManyToMany(mappedBy = "tags")
-    private Set<Inventories> inventories = new HashSet<>();
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private Set<InventoryTagRelations> inventoryRelations = new HashSet<>();
 
-    @Builder
+    // Konstruktor
     public Tags() {
-        this.inventories = new HashSet<>();
-    }
 
-    @Builder
+    }
     public Tags(String name) {
         this.name = name;
+    }
 
-        inventories = new HashSet<>();
+    // Getter und Setter
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Set<InventoryTagRelations> getInventoryRelations() {
+        return inventoryRelations;
+    }
+    public void setInventoryRelations(Set<InventoryTagRelations> inventoryRelations) {
+        this.inventoryRelations = inventoryRelations;
+    }
+    public void addInventoryRelation(InventoryTagRelations inventoryTagRelations) {
+        this.inventoryRelations.add(inventoryTagRelations);
+        inventoryTagRelations.setTag(this);
+    }
+    public void removeInventoryRelation(InventoryTagRelations inventoryTagRelations) {
+        this.inventoryRelations.remove(inventoryTagRelations);
+        inventoryTagRelations.setTag(null);
     }
 }
