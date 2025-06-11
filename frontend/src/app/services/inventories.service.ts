780a861b16129d2caef84fe8d81632016d6838dc
Throin <xthroin@gmail.com>
2025-06-11 22:09:14 +0200

    feat: Add new methods to inventories service
    - Add methods to create and update inventory items.
    
    - Add method to delete comments.
    
    - Refactor inventory and comment management methods in InventoriesService

--- frontend/src/app/services/inventories.service.ts
+++ frontend/src/app/services/inventories.service.ts
@@ -36,28 +36,41 @@ export class InventoriesService {
     return this.http.get<Inventories>(this.url, { params: params });
   }
 
+  // Inventory Item
   getInventoryById(id: number): Observable<InventoryItem> {
     return this.http.get<InventoryItem>(`${this.url}/${id}`);
   }
 
+  addInventoryItem(item: InventoryItem) {
+    return this.http.post<InventoryItem>(this.url, item);
+  }
+
+  updateInventoryById(id: number, item: InventoryItem) {
+    return this.http.patch<InventoryItem>(`${this.url}/${id}`, item);
+  }
+
+  // Comments
   getCommentsForId(id: number): Observable<Comment[]> {
     return this.http.get<any>(`${this.url}/${id}/comments`);
   }
 
+  addCommentToId(id: number, comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(`${this.url}/${id}/comments`, comment);
+  }
+
+  deleteCommentFromId(id: number, commentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}/comments/${commentId}`);
+  }
+
+  // Extensions
   getExtensionsForId(id: number): Observable<Extension[]> {
     return this.http.get<any>(`${this.url}/${id}/extensions`);
   }
 
+  // Changes
   getChangesForId(id: number): Observable<any> {
     return this.http.get<any>(`${this.url}/${id}/changes`);
   }
 
-  addCommentToId(id: number, commentDescription: string): Observable<Comment> {
-    const comment: Comment = {
-      description: commentDescription,
-      author: 'CURRENT_USER', // Replace with actual user logic
-    }
-    return this.http.post<Comment>(`${this.url}/${id}/comments`, comment);
-  }
 
 }
