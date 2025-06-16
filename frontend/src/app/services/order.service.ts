003cddb42ffcdc4ac9700877a23fc78de6a9b041
Thr0in <xthroin@gmail.com>
2025-06-17 00:21:17 +0200

    refactor: Update getOrderArticleByIds method to ensure consistent return type and improve error handling

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -16,13 +16,18 @@ export class OrderService {
     return this.http.get<Order[]>(this.url);
   }
 
-  getOrderArticleByIds(orderId: number, articleId: number): Observable<Article | undefined> {
-    return new Observable<Article | undefined>((observer) => {
+  getOrderArticleByIds(orderId: number, articleId: number): Observable<Article> {
+    return new Observable<Article>((observer) => {
       this.http.get<Order[]>(this.url).subscribe({
         next: (orders: Order[]) => {
           const article = orders
             .find(order => order.id == orderId)
             ?.articles.find(article => article.article_id == articleId);
+          if (article === undefined) {
+            console.log(orders, orderId, articleId);
+            observer.error(new Error(`Article with ID ${articleId} not found in order ${orderId}`));
+            return;
+          }
           observer.next(article);
           observer.complete();
         },
