629328ece2be5937853499b0af8a8feec9b7dfaf
kr1pt0n05 <sandrolipinski@outlook.com>
2025-06-17 21:34:42 +0200

    Production ready setup

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // This is the environment configuration for the frontend application.
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://insy.hs-esslingen.com:8080',
 }
