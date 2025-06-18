f9647ca57c68242b51f6c0ea9fc9c17c55a83eb0
kr1pt0n05 <sandrolipinski@outlook.com>
2025-06-18 17:14:46 +0200

    Fix

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // This is the environment configuration for the frontend application.
-    apiUrl: 'https://api.hs-esslingen.com',
+    apiUrl: 'https://api.insy.hs-esslingen.com',
 }
