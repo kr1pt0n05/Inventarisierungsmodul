bade28e37d75c93f99e0f9e8454d6a29aead6217
Throin <xthroin@gmail.com>
2025-06-21 23:18:07 +0200

    hotfix: Update API URL in environment configuration
    Removed the port number from the apiUrl in the frontend environment configuration to use the default port.

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // This is the environment configuration for the frontend application.
     // apiUrl: 'http://localhost:8080',
-    apiUrl: 'http://api.insy.hs-esslingen.com:8080'
+    apiUrl: 'http://api.insy.hs-esslingen.com'
 }
