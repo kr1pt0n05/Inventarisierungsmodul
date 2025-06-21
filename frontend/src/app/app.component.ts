450cb9465296c33bef787f7235dd0e1293eaba24
Throin <xthroin@gmail.com>
2025-06-21 02:38:01 +0200

    refactor: Refactor button classes in app component
    Centralized button and active state classes for navigation links into component properties. This improves maintainability and consistency across the navigation bar.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -21,6 +21,9 @@ export class AppComponent {
 
   currentPageTitle = signal<string>("Startseite");
 
+  buttonClasses = 'w-fit rounded-md px-3 py-2 text-md font-medium text-gray-300 hover:bg-gray-700 hover:text-white';
+  buttonActiveClasses = 'bg-gray-900 text-white';
+
   updateCurrentPageTitle(currentPageTitle: string) {
     this.currentPageTitle.set(currentPageTitle);
   }
