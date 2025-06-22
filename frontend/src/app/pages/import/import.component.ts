eed1e9f6d70bf56c0410a8988d32c035847dfe7e
Thr0in <xthroin@gmail.com>
2025-06-23 00:43:05 +0200

    feat: Add notifications for file upload status
    Introduced MatSnackBar to provide user feedback on file upload success or error in ImportComponent. Users now receive a notification upon successful upload or if an error occurs.

--- frontend/src/app/pages/import/import.component.ts
+++ frontend/src/app/pages/import/import.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import {CardComponent} from '../../components/card/card.component';
-import {UploadService} from '../../services/upload.service';
-import {MatButton} from '@angular/material/button';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CardComponent } from '../../components/card/card.component';
+import { UploadService } from '../../services/upload.service';
 
 
 interface File {
@@ -20,6 +21,8 @@ interface File {
   styleUrl: './import.component.css'
 })
 export class ImportComponent {
+
+  private readonly _snackBar = inject(MatSnackBar);
 
   constructor(private api: UploadService) {
   }
@@ -41,15 +44,21 @@ export class ImportComponent {
   }
 
   upload() {
-    if(this.file) this.api.uploadExcel(this.file.file).subscribe({
+    if (this.file) this.api.uploadExcel(this.file.file).subscribe({
       next: (data) => {
         console.log(data);
       },
       error: (err) => {
         console.log(err);
+        this._snackBar.open('Error uploading file', 'Close', {
+          duration: 3000,
+        });
       },
       complete: () => {
         console.log('complete');
+        this._snackBar.open('File uploaded successfully', 'Close', {
+          duration: 3000,
+        });
       }
     });
   }
