d245569245fd6b26a6df01c84df37e609bd8a234
Throin <xthroin@gmail.com>
2025-06-24 20:29:00 +0200

    feat: Add filter reset to inventory page
    Introduces a 'Reset' button to the inventory filter UI, which resets all filter form controls and range sliders to their default values. Implements a resetFilters method in InventoryComponent and a resetSlider method in RangeSliderComponent to support this feature.

--- frontend/src/app/components/range-slider/range-slider.component.ts
+++ frontend/src/app/components/range-slider/range-slider.component.ts
@@ -1,8 +1,8 @@
-import {Component, effect, input, signal} from '@angular/core';
-import {MatSlider, MatSliderRangeThumb} from '@angular/material/slider';
-import {MatFormField} from '@angular/material/form-field';
-import {MatInput, MatLabel} from '@angular/material/input';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Component, effect, input, signal } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput, MatLabel } from '@angular/material/input';
+import { MatSlider, MatSliderRangeThumb } from '@angular/material/slider';
 
 @Component({
   selector: 'app-range-slider',
@@ -40,5 +40,12 @@ export class RangeSliderComponent {
 
   leftValue = signal<number>(0);
   rightValue = signal<number>(1000);
+
+  resetSlider() {
+    this.leftValue.set(this.minValue());
+    this.rightValue.set(this.maxValue());
+    this.controlMin().setValue(this.minValue());
+    this.controlMax().setValue(this.maxValue());
+  }
 
 }
