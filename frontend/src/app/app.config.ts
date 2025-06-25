f69e24d593bee15234d902c157e15614b2a44032
kr1pt0n05 <sandrolipinski@outlook.com>
2025-06-25 21:46:42 +0200

    Remove debugInformation of OAuth Flow in frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -55,6 +55,4 @@ export const authCodeFlowConfig: AuthConfig = {
   // Important: Request offline_access to get a refresh token
   // The api scope is a usecase specific one
   scope: 'profile email offline_access',
-
-  showDebugInformation: true,
 };
