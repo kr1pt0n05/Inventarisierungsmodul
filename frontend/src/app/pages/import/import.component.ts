719936eff030dab70fef26dfed1aacc8cfed8e97
Thr0in <xthroin@gmail.com>
2025-06-26 02:20:42 +0200

    feat: Improve import UI and add processing indicator
    Refactored the import component HTML for better layout and styling, including color adjustments and improved file input presentation. In the TypeScript file, added a processing indicator dialog that opens during file upload and closes on completion or error, providing better user feedback. Also resets the file input after successful upload.

--- frontend/src/app/pages/import/import.component.ts
+++ frontend/src/app/pages/import/import.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CardComponent } from '../../components/card/card.component';
+import { ProcessingIndicatorComponent } from '../../components/processing-indicator/processing-indicator.component';
 import { UploadService } from '../../services/upload.service';
 
 
@@ -23,8 +25,9 @@ interface File {
 export class ImportComponent {
 
   private readonly _snackBar = inject(MatSnackBar);
+  readonly dialog = inject(MatDialog);
 
-  constructor(private api: UploadService) {
+  constructor(private readonly api: UploadService) {
   }
 
   file!: File | null;
@@ -44,23 +47,31 @@ export class ImportComponent {
   }
 
   upload() {
+    this.openProcessingIndicator();
     if (this.file) this.api.uploadExcel(this.file.file).subscribe({
       next: (data) => {
         console.log(data);
       },
       error: (err) => {
         console.log(err);
+        this.dialog.closeAll();
         this._snackBar.open('Error uploading file', 'Close', {
           duration: 3000,
         });
       },
       complete: () => {
         console.log('complete');
+        this.dialog.closeAll();
         this._snackBar.open('File uploaded successfully', 'Close', {
           duration: 3000,
         });
+        this.file = null;
       }
     });
+  }
+
+  openProcessingIndicator(): void {
+    this.dialog.open(ProcessingIndicatorComponent);
   }
 
 }
